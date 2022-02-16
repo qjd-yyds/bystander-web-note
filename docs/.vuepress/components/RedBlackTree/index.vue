b1bef30292d48eb681654415c120126be856ac9f
qjd <qjd1774@163.com>
2022-02-16 19:19:59 +0800

    feat: 红黑树插入情况

---
+++ docs/.vuepress/components/RedBlackTree/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">测试页面</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+
+export default defineComponent({
+  name: 'index',
+  props: {},
+  setup: () => {
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.box {
+  display: flex;
+  align-items: center;
+}
+</style>
